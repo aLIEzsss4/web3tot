a43d809bf3f8ce0800ca54dd50def6c658b4631f
aLIEzsss4 <vuetomato@gmail.com>
2022-01-05 18:08:52 +0800

    change game net form herokuapp to colyseus

--- src/game/services/Network.ts
+++ src/game/services/Network.ts
@@ -20,7 +20,9 @@ export default class Network {
 
   constructor() {
     const protocol = window.location.protocol.replace('http', 'ws')
-    const endpoint = `wss://web3tot.herokuapp.com`
+    const endpoint = `ws://hm-wmp.colyseus.in`
+    // const endpoint = `ws://localhost:2567`
+    // const endpoint = `wss://web3tot.herokuapp.com`
     // const endpoint =
     //   process.env.NODE_ENV === 'production'
     //     ? `wss://sky-office.herokuapp.com`
